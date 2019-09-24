84beae70f604b8ffd665de246625f7906589c07e
Johnny <codrkai@mbp2019.local>
2019-09-24 15:40:45 -0700

    added footer script

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(function() {
+    var str = '<div><ul id="social-icons"><li><img src="img/ig-icon.jpg" border="0" width="30" /></li><li><img src="img/fb-icon.jpg" border="0" width="30" /></li><li><img src="img/mail-icon.jpg" border="0" width="30" /></li></ul></div><div>johnnyintheoc.github.io &copy; 2019</div>';
+    $('#footer-block').html(str);
+});
