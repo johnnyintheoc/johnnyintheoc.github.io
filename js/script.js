a2848e7597917142073aec933c50f6b1c533ad90
Johnny <codrkai@mbp2019.local>
2019-09-24 18:55:03 -0700

    added header js script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function() {
     var str = '<div><ul id="social-icons"><li><img src="img/ig-icon.jpg" border="0" width="30" /></li><li><img src="img/fb-icon.jpg" border="0" width="30" /></li><li><img src="img/mail-icon.jpg" border="0" width="30" /></li></ul></div><div>johnnyintheoc.github.io &copy; 2019</div>';
     $('#footer-block').html(str);
+    $('#header-block').load("https://johnnyintheoc.github.io/header.html");
 });
