6975fb4c20e7ff778b1428377ff9e4ec2b3c6d45
Johnny <johnnyoc@gmail.com>
2019-10-10 00:58:22 -0700

    updated design layout

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(function() {
     $('#footer-block').load('https://johnnyintheoc.github.io/footer.html');
+    
     $('#header-block').load('https://johnnyintheoc.github.io/header.html');
+    
+    $('.burger-menu ul').on('click', function() {
+        $('.side-nav').addClass('active');
+    });
+
+    $(document).mouseup(function(e) {
+        let container = $('.side-nav');
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $('.side-nav').removeClass('active');
+        }
+    });
 });
