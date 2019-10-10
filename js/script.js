d16f25b9f61a89cc0d0b4770459a87ee5edd4ed7
Johnny <johnnyoc@gmail.com>
2019-10-10 16:39:32 -0700

    js side menu update

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ $(function() {
     $('#footer-block').load('https://johnnyintheoc.github.io/footer.html');
     
     $('#header-block').load('https://johnnyintheoc.github.io/header.html');
-    
-    $('.burger-menu ul').on('click', function() {
+
+    $('.burger-menu ul').mouseup(function(e) {
         $('.side-nav').addClass('active');
     });
 
-    $(document).click(function(e) {
+    $(document).mouseup(function(e) {
         var container = $('.side-nav');
         if (!container.is(e.target) && container.has(e.target).length === 0) {
             $('.side-nav').removeClass('active');
