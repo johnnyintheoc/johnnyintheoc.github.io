ea0f86f450331ad49e23175ce0dfd7b2ded5eba9
Johnny <johnnyoc@gmail.com>
2019-10-10 16:53:51 -0700

    updated js for mobile

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(function() {
     
     $('#header-block').load('https://johnnyintheoc.github.io/header.html');
 
-    $('.burger-menu ul').mouseup(function(e) {
+    $('.burger-menu ul').click(function(e) {
         $('.side-nav').addClass('active');
     });
 
