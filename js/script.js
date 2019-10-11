fd6c5db79bd14f7fd2bc23ab2e49e3d5736f4da4
Johnny <johnnyoc@gmail.com>
2019-10-10 17:43:09 -0700

    updated mobile js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
 $(function() {
     $('#footer-block').load('https://johnnyintheoc.github.io/footer.html');
     $('#header-block').load('https://johnnyintheoc.github.io/header.html');
-    
-    $('.burger-menu ul').click(function(e) {
-        //e.preventDefault();
-        $('.side-nav').addClass('active');
-    });
-
-    $(document).mouseup(function(e) {
-        var container = $('.side-nav');
-        if (!container.is(e.target) && container.has(e.target).length === 0) {
-            $('.side-nav').removeClass('active');
-        }
-    });
 });
