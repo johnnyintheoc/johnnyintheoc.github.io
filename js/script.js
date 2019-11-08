ce44563a0d5e5cfb2b79e2f475e4d4c55c47bdb6
Johnny <johnnyoc@gmail.com>
2019-11-07 17:53:25 -0800

    linked js script

--- js/script.js
+++ js/script.js
@@ -1,54 +1,31 @@
 $(function() {
-    $('#footer-block').load('https://johnnyintheoc.github.io/footer.html');
-    $('#header-block').load('https://johnnyintheoc.github.io/header.html');
+    $(".nav-link").on('click', function(e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+            var hash = this.hash;
 
-    $('.play-portfolio').bind('click', function(e) {
-        e.preventDefault();
-        $('section').addClass('no-opacity');
-        $('#portfolio-vid').html('<video controls autoplay><source src="img/portfolio-intro-480p.mov" type="video/mp4">Your browser does not support the video tag.</video>');
-    });
+            if (hash == "#contactAnchor") {
+                $('#txt-view-email').html('E-Mail: johnnyoc@gmail.com');
+            }
 
-    $('.grid img').bind('click', function(e) {
-        e.preventDefault();
-        const img = document.createElement('img');
-        img.src = this.src;
-        $('#lightbox img:last-child').remove();
-        $('#lightbox').append(img);
-        $('#lightbox').addClass('active');
-    });
-
-    $('#lightbox').bind('click', function(e) {
-        closeLightbox(e);
-    });
-
-    $('.close').bind('click', function(e) {
-        e.preventDefault();
-        closeLightbox(e);
-    });
-
-    $('input[name=iphone-submit]').bind('click', function(e) {
-        getIphoneTxt();
-    });
-
-    $('input[name=iphone-txt]').on('keypress', function(e) {
-        if (e.which == 13) {
-            getIphoneTxt();
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
         }
     });
+    
+    $('.btn-view-email').on('click', function(e) {
+        e.preventDefault();
+        $('#txt-view-email').html('E-Mail: johnnyoc@gmail.com');
+    });
 
-    function getIphoneTxt() {
-        const dte = new Date();
-        const msg = $('input[name=iphone-txt]').val();
-        if (msg != '') {
-            const txt = '<span class="iphone-txt-bubble">' + msg + '</span> <span class="iphone-date">' + dte.toLocaleString() + '</span><br /><br />';
-            $('.iphone-output').append(txt);
-            $('.iphone-txt-bubble').fadeIn('slow');
-            $('input[name=iphone-txt]').val('');
-        }
-    }
+    $('#portfolioVideoModal').on('show.bs.modal', function (e) {
+        $('#portfolioVideoModal .modal-body').html('<video controls autoplay><source src="./img/portfolio-intro-480p.mov" type="video/mp4">Your browser does not support the video tag.</video>');
+    });
 
-    function closeLightbox(e) {
-        if (e.target !== e.currentTarget) return;
-        $('#lightbox').removeClass('active');
-    }
+    $('#portfolioVideoModal').on('hide.bs.modal', function (e) {
+        $('#portfolioVideoModal .modal-body').html('');
+    });
 });
